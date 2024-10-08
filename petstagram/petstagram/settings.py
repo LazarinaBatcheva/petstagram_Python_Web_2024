481be800842c9446b984807c74cc38b9987ed147
Lazarina Batcheva <lazarina.batcheva@gmail.com>
2024-10-08 16:14:31 +0200

    feat: add 4 new apps, templates and static files

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "petstagram.photos.apps.PhotosConfig",
+    "petstagram.pets.apps.PetsConfig",
+    "petstagram.accounts.apps.AccountsConfig",
+    "petstagram.common.apps.CommonConfig"
 ]
 
 MIDDLEWARE = [
