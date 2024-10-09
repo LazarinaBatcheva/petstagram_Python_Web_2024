a6bab93a1ba1f997f9ac1c246f03b1b4b0f57d23
Lazarina Batcheva <lazarina.batcheva@gmail.com>
2024-10-09 14:28:00 +0200

    feat: add photo model and register the model to the admin site

--- petstagram/petstagram/photos/models.py
+++ petstagram/petstagram/photos/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
+from petstagram.pets.models import Pet
+from petstagram.photos.validators import FileSizeValidator
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        upload_to='mediafiles',
+        null=False,
+        blank=False,
+        validators=[
+            FileSizeValidator(5),
+        ],
+    )
+
+    description = models.TextField(
+        max_length=300,
+        validators=[
+            MinLengthValidator(10),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    location = models.CharField(
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        to=Pet,
+        related_name='tagged_pets_photos',
+        blank=True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now_add=True,
+    )
